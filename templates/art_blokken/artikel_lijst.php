0cfe19343f726a95c54ec458f71b13c7ebffb161
Lennie132 <lennart.v@live.nl>
2016-10-06 10:48:44 +0200

    added blokken module for home-page

---
+++ templates/art_blokken/artikel_lijst.php
@@ -0,0 +1,256 @@
+<?php
+$config = array(
+    'tabelnaam' => $tabelnaam,
+    'group_id' => $DATA['group'],
+    'order' => 'a.gewicht ASC', // bv: RAND()
+    'artikel_id' => 0,
+    'limit_links' => 0,
+    'where' => ''
+);
+
+/* Artikelen ophalen */
+smart_include_css('css/style.less');
+smart_include_js('script.js');
+
+$art_arr = get_artikelen_arr($config['tabelnaam'], $config['group_id'], $config['order'], $config['artikel_id'], $config['limit_links'], $config['where']);
+//print_pre($art_arr);
+if (!empty($art_arr)) {
+    ?>
+    <div class="">
+        <?php foreach ($art_arr as $key => $artikel) {
+          $img = get_art_file_path($artikel['afbeelding'], $artikel['artikel_id']);
+          $icon = get_art_file_path($artikel['icon'], $artikel['artikel_id']);
+          switch ($artikel['soort']) {
+                    case 1:
+                    // Afbeelding en tekst
+                        //--- 2x2
+                        switch ($artikel['grootte_blok']) {
+                          case '1':
+                            $width = 'col-lg-2 col-md-2 col-sm-6 col-xs-12';
+                            $height = 'block--small';
+                            break;
+                            case '2':
+                              $col = 'col-lg-2 col-md-2 col-sm-6 col-xs-12';
+                              $height = 'block--middle';
+                              break;
+                              case '3':
+                              $col = 'col-lg-4 col-md-4 col-sm-6 col-xs-12';
+                              $height = 'block--large';
+                                break;
+                          default:
+                            $col = 'col-lg-2 col-md-2 col-sm-6 col-xs-12';
+                            $height = 'block--middle';
+                            break;
+                        }
+
+                        switch ($artikel['kleur_blok']) {
+                          case 'blauw':
+                            $colour = 'block--blue';
+                            break;
+                            case 'roze':
+                              $colour = 'block--pink';
+                              break;
+                              case 'turquoise':
+                                $colour = 'block--turquoise';
+                                break;
+                          default:
+                            $colour = 'block--blue';
+                            break;
+                        }
+                        ?>
+                        <div class="block grid-item <?= $col; ?> ">
+                                                    <a class="block__link-wrapper" href="<?php link::c($artikel['link']); ?>">
+                            <div class="block__wrapper block--style-1 <?= $height; ?> <?= $colour; ?>">
+                              <?php if ($img != '') { ?>
+                                                              <div class="block__image" style="background-image: url('<?php echo lcms::resize($img, 800, 800, '', 80); ?>');"></div>
+                              <?php } ?>
+                              <div class="block__content">
+                                <p class="block__introduction">
+                                  <?= $artikel['tekst']; ?>
+                                </p>
+                              </div>
+                            </div>
+
+                          </a>
+                          </div>
+                        <?php
+                        break;
+
+
+
+
+                  case 2:
+                    // Icon en tekst
+                    switch ($artikel['grootte_blok']) {
+                      case '1':
+                        $width = 'col-lg-2 col-md-2 col-sm-6 col-xs-12';
+                        $height = 'block--small';
+                        break;
+                        case '2':
+                          $col = 'col-lg-2 col-md-2 col-sm-6 col-xs-12';
+                          $height = 'block--middle';
+                          break;
+                          case '3':
+                          $col = 'col-lg-4 col-md-4 col-sm-6 col-xs-12';
+                          $height = 'block--large';
+                            break;
+                      default:
+                        $col = 'col-lg-2 col-md-2 col-sm-6 col-xs-12';
+                        $height = 'block--middle';
+                        break;
+                    }
+
+                    switch ($artikel['kleur_blok']) {
+                      default:
+                        $colour = 'block--white';
+                        break;
+                    }
+                    ?>
+                    <div class="block grid-item <?= $col; ?> ">
+                                                <a class="block__link-wrapper" href="<?= link::c($artikel['link']); ?>">
+                        <div class="block__wrapper block--style-2 <?= $height; ?> <?= $colour; ?>">
+
+                                                    <div class="block__content">
+                                                      <?php if ($icon != '') { ?>
+                                                        <img class="block__icon" src="<?php echo lcms::resize($icon, 60, 60, '', 80); ?>" title="<?= $artikel['titel']; ?>">
+                                                        <?php } ?>
+                                                        <h2 class="block__title"><?= $artikel['titel']; ?></h2>
+                                                      <p class="block__introduction"><?= $artikel['tekst']; ?></p>
+                                                        <p class="block__link"><?= $artikel['link_naam']; ?></p>
+                                                    </div>
+                                                  </div>
+
+                      </a>
+                      </div>
+                    <?php
+                        break;
+
+
+
+
+                    case 3:
+                    // Afbeelding en transparant tekst
+                    switch ($artikel['grootte_blok']) {
+                      case '1':
+                        $width = 'col-lg-2 col-md-2 col-sm-6 col-xs-12';
+                        $height = 'block--small';
+                        break;
+                        case '2':
+                          $col = 'col-lg-2 col-md-2 col-sm-6 col-xs-12';
+                          $height = 'block--middle';
+                          break;
+                          case '3':
+                          $col = 'col-lg-4 col-md-4 col-sm-6 col-xs-12';
+                          $height = 'block--large';
+                            break;
+                      default:
+                        $col = 'col-lg-2 col-md-2 col-sm-6 col-xs-12';
+                        $height = 'block--middle';
+                        break;
+                    }
+
+                    switch ($artikel['kleur_blok']) {
+                      case 'blauw':
+                        $colour = 'block--blue';
+                        break;
+                        case 'roze':
+                          $colour = 'block--pink';
+                          break;
+                          case 'turquoise':
+                            $colour = 'block--turquoise';
+                            break;
+                      default:
+                        $colour = 'block--blue';
+                        break;
+                    }
+                    ?>
+                    <div class="block grid-item <?= $col; ?> ">
+                                                <a class="block__link-wrapper" href="<?php link::c($artikel['link']); ?>">
+                        <div class="block__wrapper block--style-3 <?= $height; ?> <?= $colour; ?>">
+                          <div class="block__image" style="background-image: url('<?php echo lcms::resize($img, 800, 800, '', 80); ?>');"></div>
+                          <div class="block__content">
+                            <div class="block__title">
+                              <?= $artikel['tekst']; ?>
+                            </div>
+                            <div class="block__link">
+                              <?= $artikel['link_naam']; ?>
+                            </div>
+                          </div>
+                        </div>
+                      </a>
+                      </div>
+                    <?php
+                        break;
+
+
+                        case 4:
+                        // Tekst
+                        switch ($artikel['grootte_blok']) {
+                          case '1':
+                            $width = 'col-lg-2 col-md-2 col-sm-6 col-xs-12';
+                            $height = 'block--small';
+                            break;
+                            case '2':
+                              $col = 'col-lg-2 col-md-2 col-sm-6 col-xs-12';
+                              $height = 'block--middle';
+                              break;
+                              case '3':
+                              $col = 'col-lg-4 col-md-4 col-sm-6 col-xs-12';
+                              $height = 'block--large';
+                                break;
+                          default:
+                            $col = 'col-lg-2 col-md-2 col-sm-6 col-xs-12';
+                            $height = 'block--middle';
+                            break;
+                        }
+
+                        switch ($artikel['kleur_blok']) {
+                          case 'blauw':
+                            $colour = 'block--blue';
+                            break;
+                            case 'roze':
+                              $colour = 'block--pink';
+                              break;
+
+                              case 'turquoise':
+                                $colour = 'block--turquoise';
+                                break;
+
+
+                          default:
+                            $colour = 'block--blue';
+                            break;
+                        }
+                        ?>
+                        <div class="block grid-item <?= $col; ?> ">
+                          <a class="block__link-wrapper" href="<?php link::c($artikel['link']); ?>">
+                        <div class="block__wrapper block--style-4 <?= $height; ?> <?= $colour; ?>">
+
+                          <div class="block__content">
+                            <div class="block__title">
+                                <?= $artikel['titel']; ?>
+                            </div>
+                            <p class="block__introduction">
+                              <?= $artikel['tekst']; ?>
+                            </p>
+                            <div class="block__link">
+                                <?= $artikel['link_naam']; ?>
+                            </div>
+                          </div>
+
+                        </div>
+                      </a>
+                        </div>
+                        <?php
+                        break;
+
+
+
+
+                    default:
+                        break;
+                } } ?>
+    </div>
+    <?php
+}
+?>
