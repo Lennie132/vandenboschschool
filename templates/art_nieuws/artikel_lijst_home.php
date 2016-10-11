0282c7bf217393ea8da1e4113c94b4e1e1e71e1e
Lennie132 <lennart.v@live.nl>
2016-10-11 11:01:24 +0200

    added news block to homepage

--- templates/art_nieuws/artikel_lijst_home.php
+++ templates/art_nieuws/artikel_lijst_home.php
@@ -1,49 +1,40 @@
 <?php
-smart_include_css('css/style.less');
-smart_include_css('css/style.css');
-smart_include_js('main.js');
 
 $config = array(
-    'tabelnaam' => $tabelnaam,
-    'group_id' => $DATA['group'],
-    'order' => 't.datum DESC', // bv: RAND()
-    'artikel_id' => 0,
-    'limit_links' => 3,
-    'where' => ''
+  'tabelnaam' => 'art_nieuws',
+  'group_id' => $DATA['group'],
+  'order' => 't.datum DESC', // bv: RAND()
+  'artikel_id' => 0,
+  'limit_links' => 3,
+  'where' => ''
 );
 
 /* Artikelen ophalen */
 
-$art_arr = get_artikelen_arr($config['tabelnaam'], $config['group_id'], $config['order'], $config['artikel_id'], $config['limit_links'], $config['where']);
+// Style van nieuws_lijst_home staat in main->main.less
+
+$nieuws_arr = get_artikelen_arr($config['tabelnaam'], $config['group_id'], $config['order'], $config['artikel_id'], $config['limit_links'], $config['where']);
 //print_pre($art_arr);
 //print_pre($DATA);
-if (!empty($art_arr)) {
+if (!empty($nieuws_arr)) {
+  foreach ($nieuws_arr as $key => $nieuwsitem) {
+    $oldDate = $nieuwsitem['Datum'];
+    $newDate = date("d-m", strtotime($oldDate));
     ?>
-    <div class="row">
-        <?php
-        foreach ($art_arr as $key => $artikel) {
-            $oldDate = $artikel['Datum'];
-            $newDate = date("d-m", strtotime($oldDate));
-            ?>
-            <div class="">
-                <div class="newsitem__wrapper">
-                    <div class="">
-                        <h3 class="newsitem__title"><?= $newDate; ?> | <?= $artikel['Titel']; ?></h3>
-                    </div>
-                    <div class=" newsitem__intro">
-                        <p class="newsitem__paragraph"><?= $artikel['Inleiding']; ?></p>
-                    </div>
-                    <div class="">
-                        <a class="newsitem__read-more" href="<?= link::v('page_nieuws')->artikel_groep($artikel['page'])->artikel_id($artikel['artikel_id']); ?>" title="<?= $artikel['Titel']; ?>">
-                        Lees meer<span class="icon-chevron_right"></span>
-                    </a>
-                    </div>
-                </div>
-            </div>
-            <?php
-        }
-        ?>
+
+    <div class="newsitem__wrapper--home">
+      <a class="newsitem__read-more" href="<?= link::v('page_nieuws')->artikel_groep($nieuwsitem['page'])->artikel_id($nieuwsitem['artikel_id']); ?>">
+        <h3 class="newsitem__title"><?= $nieuwsitem['Titel']; ?></h3>
+      </a>
+      <div class="newsitem__intro">
+        <p class="newsitem__paragraph"><?= $nieuwsitem['Inleiding']; ?></p>
+        <a class="newsitem__read-more" href="<?= link::v('page_nieuws')->artikel_groep($nieuwsitem['page'])->artikel_id($nieuwsitem['artikel_id']); ?>" title="<?= $nieuwsitem['Titel']; ?>">
+          Lees meer<span class="icon-chevron_right"></span>
+        </a>
+      </div>
     </div>
+
     <?php
+  }
 }
 ?>
