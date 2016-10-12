095e8206240c81389aad3e6ad694466c2c7939b3
Lennie132 <lennart.v@live.nl>
2016-10-12 14:50:28 +0200

    made home-blocks more modulair

--- templates/art_blokken/artikel_lijst.php
+++ templates/art_blokken/artikel_lijst.php
@@ -19,144 +19,160 @@ if (!empty($art_arr)) {
     $img = get_art_file_path($artikel['afbeelding'], $artikel['artikel_id']);
     $icon = get_art_file_path($artikel['icon'], $artikel['artikel_id']);
 
-    switch ($artikel['grootte_blok']) {
+    switch ($artikel['blok_grootte']) {
       case '1':
-      $width = 'col-lg-2 col-md-4 col-sm-6 col-xs-12';
+      $col = 'col-lg-2 col-md-4 col-sm-6 col-xs-12';
       $height = 'block--small';
       break;
       case '2':
-      $col = 'col-lg-2 col-md-4 col-sm-6 col-xs-12';
-      $height = 'block--middle';
-      break;
-      case '3':
       $col = 'col-lg-4 col-md-6 col-sm-6 col-xs-12';
       $height = 'block--large';
       break;
       default:
       $col = 'col-lg-2 col-md-4 col-sm-6 col-xs-12';
-      $height = 'block--middle';
+      $height = 'block--small';
       break;
     }
 
-    switch ($artikel['kleur_blok']) {
-      case 'blauw':
-      $colour = 'block--blue';
-      break;
-      case 'roze':
-      $colour = 'block--pink';
-      break;
-      case 'turquoise':
-      $colour = 'block--turquoise';
-      break;
-      default:
-      $colour = 'block--blue';
-      break;
-    }
+    switch ($artikel['blok_soort']) {
 
-    switch ($artikel['soort']) {
       case 1:
+      // Tekst (Optioneel: Icon)
+      ?>
+      <div class="block grid-item <?= $col; ?> ">
+        <div class="block__wrapper block--style-1 <?= $height; ?> <?= $artikel['blok_kleur']; ?>">
+          <div class="block__content">
+            <a href="<?= link::c($artikel['link']); ?>">
+              <?php if ($artikel['icon_toon'] == 1 && $icon != '') { ?>
+                <img class="block__icon" src="<?php echo lcms::resize($icon, 60, 60, '', 80); ?>" title="<?= $artikel['titel']; ?>">
+                <?php
+              } ?>
+              <?php if ($artikel['titel_toon'] == 1 && $artikel['titel'] != '') { ?>
+                <h2 class="block__title"><?= $artikel['titel']; ?></h2>
+                <?php
+              } ?>
+            </a>
+
+            <?php if ($artikel['tekst_toon'] == 1 && $artikel['tekst'] != '') { ?>
+              <div class="block__text">
+                <?= $artikel['tekst']; ?>
+              </div>
+              <?php
+            } ?>
+
+            <?php if ($artikel['module_toon'] == 1 && $artikel['module'] != '') { ?>
+              <div class="block__module">
+              <?php include  $artikel['module']; ?>
+              </div>
+              <?php
+            } ?>
+
+            <?php if ($artikel['link_toon'] == 1 && $artikel['link_naam'] != '') { ?>
+              <a class="block__link <?= ($artikel['link_toon_scheidinglijn'] == 1) ? 'block__link--border-top' : '' ?>" href="<?= link::c($artikel['link']); ?>"><?= $artikel['link_naam']; ?><span class="icon-chevron_right"></span></a>
+              <?php
+            } ?>
+          </div>
+        </div>
+      </div>
+      <?php
+      break;
+
+
+      case 2:
       // Afbeelding en tekst
       ?>
       <div class="block grid-item <?= $col; ?> ">
-        <a class="block__link-wrapper" href="<?php link::c($artikel['link']); ?>">
-          <div class="block__wrapper block--style-1 <?= $height; ?> <?= $colour; ?>">
+        <a class="block__link-wrapper" href="<?= link::c($artikel['link']); ?>">
+          <div class="block__wrapper block--style-2 <?= $height; ?> <?= $artikel['blok_kleur']; ?>">
             <?php if ($img != '') { ?>
               <div class="block__image" style="background-image: url('<?php echo lcms::resize($img, 800, 800, '', 80); ?>');"></div>
-              <?php } ?>
+              <?php
+            }
+            ?>
+          </a>
+          <div class="block__content">
+            <a href="<?= link::c($artikel['link']); ?>">
+              <?php if ($artikel['icon_toon'] == 1 && $icon != '') { ?>
+                <img class="block__icon" src="<?php echo lcms::resize($icon, 60, 60, '', 80); ?>" title="<?= $artikel['titel']; ?>">
+                <?php
+              } ?>
+              <?php if ($artikel['titel_toon'] == 1 && $artikel['titel'] != '') { ?>
+                <h2 class="block__title"><?= $artikel['titel']; ?></h2>
+                <?php
+              } ?>
             </a>
-            <div class="block__content">
-              <div class="block__introduction">
+            <?php if ($artikel['tekst_toon'] == 1 && $artikel['tekst'] != '') { ?>
+              <div class="block__text">
                 <?= $artikel['tekst']; ?>
               </div>
-            </div>
+              <?php
+            } ?>
+
+            <?php if ($artikel['module_toon'] == 1 && $artikel['module'] != '') { ?>
+              <div class="block__module">
+              <?php include  $artikel['module']; ?>
+              </div>
+              <?php
+            } ?>
+
+            <?php if ($artikel['link_toon'] == 1 && $artikel['link_naam'] != '') { ?>
+              <a class="block__link <?= ($artikel['link_toon_scheidinglijn'] == 1) ? 'block__link--border-top' : '' ?>" href="<?= link::c($artikel['link']); ?>"><?= $artikel['link_naam']; ?><span class="icon-chevron_right"></span></a>
+              <?php
+            } ?>
           </div>
-
         </div>
-        <?php
-        break;
 
+      </div>
+      <?php
+      break;
 
-        case 2:
-        // Icon en tekst
-        ?>
-        <div class="block grid-item <?= $col; ?> ">
-          <div class="block__wrapper block--style-2 <?= $height; ?> block--white">
-            <div class="block__content">
+      case 3:
+      // Afbeelding en transparant tekst
+      ?>
+      <div class="block grid-item <?= $col; ?> ">
+        <div class="block__wrapper block--style-3 <?= $height; ?> <?= $artikel['blok_kleur']; ?>">
+          <a href="<?= link::c($artikel['link']); ?>">
+            <div class="block__image" style="background-image: url('<?php echo lcms::resize($img, 800, 800, '', 80); ?>');"></div>
+          </a>
+          <div class="block__content block__content--transparant">
+            <a href="<?= link::c($artikel['link']); ?>">
+              <?php if ($artikel['icon_toon'] == 1 && $icon != '') { ?>
+                <img class="block__icon" src="<?php echo lcms::resize($icon, 60, 60, '', 80); ?>" title="<?= $artikel['titel']; ?>">
+                <?php
+              } ?>
+              <?php if ($artikel['titel_toon'] == 1 && $artikel['titel'] != '') { ?>
+                <h2 class="block__title"><?= $artikel['titel']; ?></h2>
+                <?php
+              } ?>
+            </a>
 
-              <a class="block__link-wrapper" href="<?= link::c($artikel['link']); ?>">
-                <?php if ($icon != '') { ?>
-                  <img class="block__icon" src="<?php echo lcms::resize($icon, 60, 60, '', 80); ?>" title="<?= $artikel['titel']; ?>">
-                  <?php } ?>
-                  <h2 class="block__title"><?= $artikel['titel']; ?></h2>
-                </a>
-
-                <?php if ($artikel['module_invoegen'] != '') {
-                  include  $artikel['module_invoegen'];
-                } else { ?>
-                  <p class="block__introduction"><?= $artikel['tekst']; ?></p>
-                  <?php } ?>
-                  <a class="block__link" href="<?= link::c($artikel['link']); ?>"><?= $artikel['link_naam']; ?><span class="icon-chevron_right"></span></a>
-                </div>
-              </div>
-
-
-            </div>
-            <?php
-            break;
-
-
-            case 3:
-            // Afbeelding en transparant tekst
-            ?>
-            <div class="block grid-item <?= $col; ?> ">
-              <div class="block__wrapper block--style-3 <?= $height; ?> <?= $colour; ?>">
-                <a class="block__link-wrapper" href="<?php link::c($artikel['link']); ?>">
-                  <div class="block__image" style="background-image: url('<?php echo lcms::resize($img, 800, 800, '', 80); ?>');"></div>
-                </a>
-                <div class="block__content">
-                  <div class="block__title">
-                    <?= $artikel['tekst']; ?>
-                  </div>
-                  <?php if ($artikel['link_naam'] != '') { ?>
-                    <div class="block__link">
-                      <a href="<?php link::c($artikel['link']); ?>"><?= $artikel['link_naam']; ?><span class="icon-chevron_right"></span></a>
-                    </div>
-                    <?php } ?>
-                  </div>
-                </div>
+            <?php if ($artikel['tekst_toon'] == 1 && $artikel['tekst'] != '') { ?>
+              <div class="block__text">
+                <?= $artikel['tekst']; ?>
               </div>
               <?php
-              break;
+            } ?>
 
-
-              case 4:
-              // Tekst
-              ?>
-              <div class="block grid-item <?= $col; ?> ">
-
-                <div class="block__wrapper block--style-4 <?= $height; ?> <?= $colour; ?>">
-                  <div class="block__content">
-                    <a class="block__title" href="<?php link::c($artikel['link']); ?>">
-                        <?= $artikel['titel']; ?>
-                    </a>
-                    <p class="block__introduction">
-                      <?= $artikel['tekst']; ?>
-                    </p>
-                    <?php if ($artikel['link_naam'] != '') { ?>
-                    <a class="block__link" href="<?php link::c($artikel['link']); ?>">
-                      <?= $artikel['link_naam']; ?><span class="icon-chevron_right"></span>
-                    </a>
-                    <?php } ?>
-                  </div>
-                </div>
-
+            <?php if ($artikel['module_toon'] == 1 && $artikel['module'] != '') { ?>
+              <div class="block__module">
+              <?php include  $artikel['module']; ?>
               </div>
               <?php
-              break;
+            } ?>
 
-              default:
-              break;
-            }
-          }
-        }
-        ?>
+            <?php if ($artikel['link_toon'] == 1 && $artikel['link_naam'] != '') { ?>
+              <a class="block__link <?= ($artikel['link_toon_scheidinglijn'] == 1) ? 'block__link--border-top' : '' ?>" href="<?= link::c($artikel['link']); ?>"><?= $artikel['link_naam']; ?><span class="icon-chevron_right"></span></a>
+              <?php
+            } ?>
+          </div>
+        </div>
+      </div>
+      <?php
+      break;
+
+      default:
+      break;
+    }
+  }
+}
+?>
