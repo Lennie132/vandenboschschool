6d49496b8671b786dffb70c180cffa77c50bd235
Lennie132 <lennart.v@live.nl>
2016-10-24 16:59:24 +0200

    added default sfeerbeeld

--- templates/paginas/contact/template.php
+++ templates/paginas/contact/template.php
@@ -1,48 +1,44 @@
 <?php
-/** Hier eventeel nog een sfeerbeeld of kruimels o.i.d. inladen **/
-$sfeer = get_sfeerafbeelding($DATA['page'], 1);
+  /** Hier eventeel nog een sfeerbeeld of kruimels o.i.d. inladen * */
+  $sfeer = get_sfeerafbeelding($DATA['page'], 1);
 
-/** Hier wordt het grid met de content ingelezen **/
+  if (trim($sfeer) != '') {
+    $src = $sfeer;
+  } else {
+    $src = 'img/default-sfeerbeeld.jpg';
+  }
+
+  /** Hier wordt het grid met de content ingelezen * */
 //echo $this->buildHTMLgrid($DATA['page']);
 
-/** Individuele content inlezen **/
+  /** Individuele content inlezen * */
 //echo lcms::Template()->getSectieContent($DATA['page'], '<id van sectie>');
 ?>
-<div class="grid-item col-md-8 col-xs-12 content">
-  <?php if (trim($sfeer) != '') {
-    ?>
-    <div class="content__sfeerbeeld" style="background-image: url('<?= lcms::resize($sfeer, 1250, 500, '', 80); ?>');">
-      <div class="content__titel-wrapper content__titel-wrapper--overlay">
-        <h1 class="content__titel"><?php echo get_pagina_title($pageid) ?></h1>
-      </div>
+<div class="grid-item content col-md-8 col-xs-12">
+
+  <div class="content__sfeerbeeld" style="background-image: url('<?= lcms::resize($src, 1250, 500, '', 80); ?>');">
+    <div class="content__titel-wrapper content__titel-wrapper--overlay">
+      <h1 class="content__titel"><?= get_pagina_title($pageid) ?></h1>
     </div>
-    <?php
-  } else {
-    ?>
-    <div class="content__titel-wrapper">
-      <h1 class="content__titel"><?php echo get_pagina_title($pageid) ?></h1>
-    </div>
-    <?php
-  }
-  ?>
+  </div>
 
   <div class="content__main">
     <div class="row">
       <div class="col-md-6">
         <?php
-        /** Hier wordt het linker deel ingelezen * */
-        echo lcms::Template()->getSectieContent($DATA['page'], 'links');
+          /** Hier wordt het linker deel ingelezen * */
+          echo lcms::Template()->getSectieContent($DATA['page'], 'links');
         ?>
       </div>
       <div class="col-md-6">
         <?php
-        /** Hier wordt het rechter deel ingelezen * */
-        echo lcms::Template()->getSectieContent($DATA['page'], 'rechts');
+          /** Hier wordt het rechter deel ingelezen * */
+          echo lcms::Template()->getSectieContent($DATA['page'], 'rechts');
         ?>
       </div>
     </div>
-
   </div>
+
   <!-- Roze bar onderin -->
   <div class="content__bottom-bar"></div>
 </div>
--- templates/paginas/standaard/template.php
+++ templates/paginas/standaard/template.php
@@ -1,37 +1,34 @@
 <?php
-/** Hier eventeel nog een sfeerbeeld of kruimels o.i.d. inladen **/
-$sfeer = get_sfeerafbeelding($DATA['page'], 1);
+  /** Hier eventeel nog een sfeerbeeld of kruimels o.i.d. inladen * */
+  $sfeer = get_sfeerafbeelding($DATA['page'], 1);
 
-/** Hier wordt het grid met de content ingelezen **/
+  if (trim($sfeer) != '') {
+    $src = $sfeer;
+  } else {
+    $src = 'img/default-sfeerbeeld.jpg';
+  }
+
+  /** Hier wordt het grid met de content ingelezen * */
 //echo $this->buildHTMLgrid($DATA['page']);
 
-/** Individuele content inlezen **/
+  /** Individuele content inlezen * */
 //echo lcms::Template()->getSectieContent($DATA['page'], '<id van sectie>');
 ?>
-<div class="grid-item col-md-8 col-xs-12 content">
-  <?php if (trim($sfeer) != '') {
-    ?>
-    <div class="content__sfeerbeeld" style="background-image: url('<?= lcms::resize($sfeer, 1250, 500, '', 80); ?>');">
-      <div class="content__titel-wrapper content__titel-wrapper--overlay">
-        <h1 class="content__titel"><?php echo get_pagina_title($pageid) ?></h1>
-      </div>
+<div class="grid-item content col-md-8 col-xs-12">
+
+  <div class="content__sfeerbeeld" style="background-image: url('<?= lcms::resize($src, 1250, 500, '', 80); ?>');">
+    <div class="content__titel-wrapper content__titel-wrapper--overlay">
+      <h1 class="content__titel"><?= get_pagina_title($pageid) ?></h1>
     </div>
-    <?php
-  } else {
-    ?>
-    <div class="content__titel-wrapper">
-      <h1 class="content__titel"><?php echo get_pagina_title($pageid) ?></h1>
-    </div>
-    <?php
-  }
-  ?>
+  </div>
 
   <div class="content__main">
     <?php
-    /** Hier wordt het grid met de content ingelezen * */
-    echo get_all_content_html();
+      /** Hier wordt het grid met de content ingelezen * */
+      echo get_all_content_html();
     ?>
   </div>
+
   <!-- Roze bar onderin -->
   <div class="content__bottom-bar"></div>
 </div>
